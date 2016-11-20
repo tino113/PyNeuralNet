d70f5b62a382b8202a76be3b060ee5f6d4df396f
Dan Brooks <danger.dan_@hotmail.co.uk>
2016-11-20 23:23:20 +0000

    Refactor axioms to use constructor

--- py/axiom.py
+++ py/axiom.py
@@ -1,21 +1,14 @@
 
-
 class Axiom:
     """Weighted Links which feed data between nodes"""
 
-    def __init__(self):
-        self.nodeFrom = 0
-        self.nodeTo = 0
-        self.weight = 0
+    def __init__(self, nodeFrom, nodeTo, weight):
+        self.weight = weight
+        self.nodeFrom = nodeFrom
+        self.nodeTo = nodeTo
 
     def print(self):
         print("axiom: " + str(self) + " from: " + str(self.nodeFrom) + " to: " + str(self.nodeTo) + " weight: " + str(self.weight))
-
-    def create(self,nFrom,nTo,w):
-        self.weight = w
-        self.nodeFrom = nFrom
-        self.nodeTo = nTo
-        return self
 
     def getData(self):
         return self.nodeFrom.data() * self.weight
